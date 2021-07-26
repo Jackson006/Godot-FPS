bd0a46022319b90aa10dfb29c21e74ec9ae221b8
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2021-07-27 09:58:26 +1000

    Update Weapon_Rifle.gd

--- Godot_FPS_Starter/Weapon_Rifle.gd
+++ Godot_FPS_Starter/Weapon_Rifle.gd
@@ -5,6 +5,11 @@ const DAMAGE = 4
 const IDLE_ANIM_NAME = "Rifle_idle"
 const FIRE_ANIM_NAME = "Rifle_fire"
 
+const CAN_RELOAD = true
+const CAN_REFILL = true
+
+const RELOADING_ANIM_NAME = "Rifle_reload"
+
 var is_weapon_enabled = false
 
 var player_node = null
@@ -47,6 +52,31 @@ func unequip_weapon():
 
 	if player_node.animation_manager.current_state == "Idle_unarmed":
 		is_weapon_enabled = false
+		return true
+
+	return false
+
+func reload_weapon():
+	var can_reload = false
+
+	if player_node.animation_manager.current_state == IDLE_ANIM_NAME:
+		can_reload = true
+
+	if spare_ammo <= 0 or ammo_in_weapon == AMMO_IN_MAG:
+		can_reload = false
+
+	if can_reload == true:
+		var ammo_needed = AMMO_IN_MAG - ammo_in_weapon
+
+		if spare_ammo >= ammo_needed:
+			spare_ammo -= ammo_needed
+			ammo_in_weapon = AMMO_IN_MAG
+		else:
+			ammo_in_weapon += spare_ammo
+			spare_ammo = 0
+
+		player_node.animation_manager.set_animation(RELOADING_ANIM_NAME)
+
 		return true
 
 	return false
