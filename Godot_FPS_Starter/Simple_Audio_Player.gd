dcf44e0dd98c152e20daf0d3fafcdd010eb6d342
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2021-07-27 10:05:41 +1000

    Audio changes

---
+++ Godot_FPS_Starter/Simple_Audio_Player.gd
@@ -0,0 +1,44 @@
+extends Spatial
+
+# All of the audio files.
+# You will need to provide your own sound files.
+var audio_pistol_shot = preload("res://path_to_your_audio_here")
+var audio_gun_cock = preload("res://path_to_your_audio_here")
+var audio_rifle_shot = preload("res://path_to_your_audio_here")
+
+var audio_node = null
+
+func _ready():
+	audio_node = $Audio_Stream_Player
+	audio_node.connect("finished", self, "destroy_self")
+	audio_node.stop()
+
+
+func play_sound(sound_name, position=null):
+	if audio_pistol_shot == null or audio_rifle_shot == null or audio_gun_cock == null:
+		print ("Audio not set!")
+		queue_free()
+		return
+
+	if sound_name == "Pistol_shot":
+		audio_node.stream = audio_pistol_shot
+	elif sound_name == "Rifle_shot":
+		audio_node.stream = audio_rifle_shot
+	elif sound_name == "Gun_cock":
+		audio_node.stream = audio_gun_cock
+	else:
+		print ("UNKNOWN STREAM")
+		queue_free()
+		return
+
+# If you are using an AudioStreamPlayer3D, then uncomment these lines to set the position.
+#if audio_node is AudioStreamPlayer3D:
+#    if position != null:
+#        audio_node.global_transform.origin = position
+
+	audio_node.play()
+
+
+func destroy_self():
+	audio_node.stop()
+	queue_free()
