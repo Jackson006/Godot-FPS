c6d9aa573cdd88b61c2063fce9a2c0b58b0af9bb
Jackson006 <87291127+Jackson006@users.noreply.github.com>
2021-08-02 12:44:25 +1000

    Grenade updates, pistol ammo fix and target fix

--- Godot_FPS_Starter/Weapon_Pistol.gd
+++ Godot_FPS_Starter/Weapon_Pistol.gd
@@ -55,6 +55,10 @@ func unequip_weapon(): #checks things in reverse to equip function
 	else:
 		return false
 
+func reset_weapon():
+	ammo_in_weapon = 10
+	spare_ammo = 20
+
 func reload_weapon():
 	var can_reload = false # Checks to see if the weapon can be reloaded
 
